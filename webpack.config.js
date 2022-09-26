00835f49c303f6efa3fd822bb4e3c2ea223afa98
Prashant <prashantsoni1242@gmail.com>
2022-09-27 00:58:24 +0530

    Tailwind Integration

--- webpack.config.js
+++ webpack.config.js
@@ -1,65 +1,78 @@
 const webpack = require("webpack");
 const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
-const config = {
+module.exports = (env, argv) => {
+  var config = {
     mode: "development",
     entry: {
-        bundle: path.resolve(__dirname, "src/index.js"),
+      bundle: path.resolve(__dirname, "src/index.js"),
     },
     output: {
-        path: path.resolve(__dirname, "dist"),
-        filename: "[name][contenthash].js",
-        clean: true,
+      path: path.resolve(__dirname, "dist"),
+      filename: "[name][contenthash].js",
+      clean: true,
     },
     devServer: {
-        static: {
-            directory: path.resolve(__dirname, "dist"),
-        },
-        port: 3000,
-        open: true,
-        hot: true,
-        compress: true,
-        historyApiFallback: true,
-        watchFiles: ["index.html"],
+      static: {
+        directory: path.resolve(__dirname, "dist"),
+      },
+      port: 3000,
+      open: true,
+      hot: true,
+      compress: true,
+      historyApiFallback: true,
+      watchFiles: ["index.html"],
     },
 
     module: {
-        rules: [
+      rules: [
+        {
+          test: /\.js$/,
+          use: "babel-loader",
+          exclude: /node_modules/,
+        },
+        {
+          test: /\.css$/,
+          use: [
+            argv.mode === "production"
+              ? MiniCssExtractPlugin.loader
+              : "style-loader",
             {
-                test: /\.js$/,
-                use: "babel-loader",
-                exclude: /node_modules/,
+              loader: "css-loader",
+              options: {
+                importLoaders: 1,
+              },
             },
-            {
-                test: /\.css$/,
-                use: [
-                    "style-loader",
-                    {
-                        loader: "css-loader",
-                        options: {
-                            importLoaders: 1,
-                        },
-                    },
-                    "postcss-loader",
-                ],
-            },
-            {
-                test: /\.html$/i,
-                loader: "html-loader",
-                options: {
-                    minimize: true,
-                },
-            },
-        ],
+            "postcss-loader",
+          ],
+        },
+        {
+          test: /\.html$/i,
+          loader: "html-loader",
+          options: {
+            minimize: true,
+          },
+        },
+      ],
     },
 
     plugins: [
-        new HtmlWebpackPlugin({
-            template: "./index.html",
-            filename: "index.html",
-        }),
+      new HtmlWebpackPlugin({
+        template: "./index.html",
+        filename: "index.html",
+      }),
     ],
-};
+  };
 
-module.exports = config;
+  if (argv.mode === "production") {
+    config.plugins.push(
+      new MiniCssExtractPlugin({
+        filename: "[name][contenthash].css",
+      })
+    );
+  }
+
+  return config;
+};
